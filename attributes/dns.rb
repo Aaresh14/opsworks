7cd3ecacc69f501f90807a436d7db1ca4a11fef3
Aaresh14 <32988411+Aaresh14@users.noreply.github.com>
2018-08-07 18:56:48 -0400

    Create dns.rb

---
+++ attributes/dns.rb
@@ -0,0 +1,19 @@
+template '/tmp/dns.json' do
+  source 'dns.json.erb'
+  mode '0644'
+end
+execute "enroll in DNS" do
+  command "aws route53 change-resource-record-sets --hosted-zone-id #{node['dns_zone_id']} --change-batch file:///tmp/dns.json"
+end
+
+Chef::Log.info("****** DNS reconfig ******")
+execute 'Configure hostname2' do
+  command "echo #{node['opsworks']['instance']['hostname']}.#{node['opsworks']['stack']['name']}.#{node['domain_name']} > /etc/hostname"
+  ignore_failure true
+end
+
+
+template '/etc/sysconfig/network' do
+  source 'network.erb'
+  mode '0644'
+end
